015327e0f6d8485220a9600211980ddc8edb9920
sikandarsafdar <msikandar.safdar@gmail.com>
2024-03-18 21:21:23 +0500

    my assignment

---
+++ Javascript--course/add.js
@@ -0,0 +1,4 @@
+const PI=3.14;
+let r= 7;
+area = PI *r *r;
+console.log(area);
---
+++ Javascript--course/object.js
@@ -0,0 +1,8 @@
+const student={
+    fullName : "Sikandar",
+    age : 21,
+    cgpa:3.8,
+    ipass:true,
+
+};
+console.log(student);
